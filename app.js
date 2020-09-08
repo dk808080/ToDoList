d580320fdc71f3dd943d9c46bf55ca6fb2aef645
dk808080 <57583077+dk808080@users.noreply.github.com>
2020-09-08 15:36:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-dimpal:abhinav@dimpal@todolist.bcoon.mongodb.net/todolistDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.set('useFindAndModify', false);
 
